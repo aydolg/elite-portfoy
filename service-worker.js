f7525dc9922a807137cc143b01bf5eb26b31c36e
aydolg <ykefal@gmail.com>
2026-01-16 17:10:12 +0300

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open('elite-v1').then(c=>c.addAll([
+      '/elite-portfoy/',
+      '/elite-portfoy/index.html',
+      '/elite-portfoy/manifest.json'
+    ]))
+  );
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
