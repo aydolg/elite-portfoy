acce2ec494e9c68be464b4d2e5866274753cea12
aydolg <ykefal@gmail.com>
2026-01-16 17:26:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,28 @@
-self.addEventListener('install',e=>{
+const CACHE_NAME = "elite-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('elite-v1').then(c=>c.addAll([
-      '/elite-portfoy/',
-      '/elite-portfoy/index.html',
-      '/elite-portfoy/manifest.json'
-    ]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
-self.addEventListener('fetch',e=>{
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
